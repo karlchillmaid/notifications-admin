d8f3eb3f8202cc8c8a81ebed0fd4fb3300461243
Pea Tyczynska <pea.tyczynska@digital.cabinet-office.gov.uk>
2018-07-10 15:14:13 +0100

    Page displays a message if no results found

--- tests/app/main/views/test_find_users.py
+++ tests/app/main/views/test_find_users.py
@@ -29,9 +29,23 @@ def test_find_users_by_email_displays_users_found(
 ):
     mock_get_user(mocker, user=platform_admin_user)
     client.login(platform_admin_user)
-    mocker.patch('app.user_api_client.find_users_by_full_or_partial_email', return_value={"users": [user_json()]}, autospec=True)
+    mocker.patch('app.user_api_client.find_users_by_full_or_partial_email', return_value={"data": [user_json()]}, autospec=True)
     response = client.post(url_for('main.find_users_by_email', data=[{"email": "twilight.sparkle"}]))
     assert response.status_code == 200
 
     document = html.fromstring(response.get_data(as_text=True))
     assert "Test User" in document.text_content()
+
+def test_find_users_by_email_displays_message_if_no_users_found(
+        client,
+        platform_admin_user,
+        mocker
+):
+    mock_get_user(mocker, user=platform_admin_user)
+    client.login(platform_admin_user)
+    mocker.patch('app.user_api_client.find_users_by_full_or_partial_email', return_value={"data": []}, autospec=True)
+    response = client.post(url_for('main.find_users_by_email', data=[{"email": "twilight.sparkle"}]))
+    assert response.status_code == 200
+
+    document = html.fromstring(response.get_data(as_text=True))
+    assert "No users found." in document.text_content()
